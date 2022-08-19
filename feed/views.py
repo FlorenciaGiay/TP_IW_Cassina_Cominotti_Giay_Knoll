0d1b6e9c770ff4bed37ae72b716a86095b2d6238
MauroCominotti <maurocominotti@hotmail.com>
2022-08-19 08:09:48 -0300

    feat: first migration of post model, add boostrap, create base and entrepreneurs page

--- feed/views.py
+++ feed/views.py
@@ -1,6 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Mauro Cominotti',
+        'title': 'Blog Post 1',
+        'content': 'Third post content',
+        'date_posted': 'August 28, 2022'
+    },
+    {
+        'author': 'Florencia Giay',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2022'
+    },
+    {
+        'author': 'Paula Cassina',
+        'title': 'Blog Post 3',
+        'content': 'First post content',
+        'date_posted': 'August 28, 2022'
+    },
+    {
+        'author': 'Gonzalo Knoll',
+        'title': 'Blog Post 4',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2022'
+    }
+]
+
+
 def home(request):
-  return render(request, 'feed/home.html')
-  # return HTTPResponse('<h1>Hello Feed!!</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'feed/home.html', context)
+
+
+def entrepreneurs(request):
+    return render(request, 'feed/entrepreneurs.html')
