26cb046c76d995f35c3b0121c68762bbedec200c
MauroCominotti <maurocominotti@hotmail.com>
2022-08-22 11:04:49 -0300

    feat: custom model of user

--- rafaela_emprende/settings.py
+++ rafaela_emprende/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  
+MAILER_EMAIL_BACKEND = EMAIL_BACKEND  
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
@@ -36,6 +37,8 @@ EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 
 ALLOWED_HOSTS = ['https://rafaela-emprende.herokuapp.com/']
 
+# For overwriting users default model
+AUTH_USER_MODEL = 'users.User'
 
 # Application definition
 
