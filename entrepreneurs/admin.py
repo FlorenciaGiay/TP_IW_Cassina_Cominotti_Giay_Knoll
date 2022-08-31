da1271d55421137a2a58c769ee918c1c88c9c1bc
MauroCominotti <maurocominotti@hotmail.com>
2022-08-31 08:26:03 -0300

    refactor: finish model of entrepreneurs with categories and status with initialization in migrations

--- entrepreneurs/admin.py
+++ entrepreneurs/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from entrepreneurs.models import Entrepreneur 
+from entrepreneurs.models import Entrepreneur, EntrepreneurCategory, EntrepreneurStatus
 
 admin.site.register(Entrepreneur)
+admin.site.register(EntrepreneurCategory)
+admin.site.register(EntrepreneurStatus)
--- entrepreneurs/models.py
+++ entrepreneurs/models.py
@@ -2,9 +2,23 @@ from django.db import models
 from users.models import User
 
 
+class EntrepreneurStatus(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.description
+
+class EntrepreneurCategory(models.Model):
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.description
+
 class Entrepreneur(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, blank=True, null=True, on_delete=models.CASCADE)
     entrepreneurship_name = models.CharField(max_length=100)
     entrepreneurship_email = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
     description = models.TextField()
+    status = models.ForeignKey('EntrepreneurStatus', null=False, blank=False, on_delete=models.PROTECT)
+    category = models.ForeignKey('EntrepreneurCategory', null=False, blank=False, on_delete=models.PROTECT)
