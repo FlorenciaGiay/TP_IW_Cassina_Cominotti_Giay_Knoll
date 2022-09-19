9c091b6ce0f137e81f8ef5fbd8ba19b92b66dd75
MauroCominotti <maurocominotti@hotmail.com>
2022-09-19 19:31:04 -0300

    feat: add minty theme for bootstrap, improve pagination, change event filters

--- feed/filter.py
+++ feed/filter.py
@@ -1,8 +1,9 @@
 from django_filters import FilterSet
 from .models import Event
+from django import forms
 
 
 class EventFilter(FilterSet):
     class Meta:
         model = Event
-        fields = ["created_at"]
+        fields = ["title", "cost_of_entry"]
